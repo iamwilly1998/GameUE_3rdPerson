8a6d044c169ae44e74c35ba1aff3561dec779e86
Minh Hoang <nguyenduyminhhoang@gmail.com>
2024-12-30 12:17:21 +0700

    add combo attack

---
+++ Source/GameGocNhinThu3/Public/AnimNotifies/Combo_AN.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "Combo_AN.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEGOCNHINTHU3_API UCombo_AN : public UAnimNotify
+{
+	GENERATED_BODY()
+	
+};
